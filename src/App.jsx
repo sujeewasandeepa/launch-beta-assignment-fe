e8b30a541de036a2ca7a2d330858fa5f84150afb
L.A.S.S. Kularathna <sujeewasandeepa@protonmail.ch>
2023-05-13 10:11:25 +0530

    create basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
 
+  const [employees, setEmployees] = useState(0);
+
   return (
     <>
-      <h1 className='has-text-danger'>hello</h1>
+      <h1 className='has-text-dark m-5 title'>People</h1>
+      <hr />
     </>
   )
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,23 @@
+export default function Table() {
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Display Name</th>
+            <th>Emp ID</th>
+            <th>Designation</th>
+            <th>Emp.type</th>
+            <th>Experience</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
